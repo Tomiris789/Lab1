52370e5e63503d55a64dfcadc9af64bf12f05b8f
Tomiris789 <bolatbekova.t@gmail.com>
2017-02-13 00:25:44 +0600

    Update FileSystem.cs

--- ConsoleApplication/Week I/FileSystem.cs
+++ ConsoleApplication/Week I/FileSystem.cs
@@ -7,21 +7,22 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication.Week_II
 {
-    public static class FileSystem
+    public static class FileSystem  //класс FileSystem
     {
-        public static string F2()
+        public static string F2()   
         {
-            FileStream fs = new FileStream(@"input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            StreamReader sr = new StreamReader(fs);
+            FileStream fs = new FileStream(@"input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite); //FileStream используется для операции чтения и записи в файл, открытия и закрытия файлов в файловой системе
+            //FileMode- определяет способ открыти или создания файла; FileAccess- определяет каким образом FileStream может получить доступ к файлу
+            StreamReader sr = new StreamReader(fs); //StreamReader- считывает символы из потока байтов в определенной кодировке
 
-            string text = sr.ReadToEnd();
+            string text = sr.ReadToEnd();  //строка text считывает файл. ReadToEnd-считывает все символы начиная с текущей позиции до конца потока
 
             
 
-            sr.Close();
-            fs.Close();
+            sr.Close();  //закрывает объект StreamReader 
+            fs.Close();   //закрывает текущий поток
 
-            return text;
+            return text; //возвращаем данные с файла
         }
     }
 }
